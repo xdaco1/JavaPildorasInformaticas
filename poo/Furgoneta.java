42a8380593c7b5c882e35e85fdef5857d4d52bb5
Daniel <xdaco@hotmail.com>
2019-01-04 14:28:57 -0500

    Clase 41 Herencia

---
+++ poo/Furgoneta.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Furgoneta extends Coche{
+	
+	private int capacidadCarga;
+	private int plazasExtra;
+	
+	public Furgoneta(int capacidadCarga, int plazasExtra) {
+		
+		super(); //Llama al constructor de la súper clase (clase padre)
+		
+		this.capacidadCarga = capacidadCarga;
+		this.plazasExtra 	= plazasExtra;
+		
+	}
+	
+}
