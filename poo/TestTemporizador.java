d1ec2a42da5dba72192f4f8c69ce624695014814
Daniel <xdaco@hotmail.com>
2019-01-14 15:15:55 -0500

    Clase 52: Interfaces y clases internas. Temporizador Eventos.

---
+++ poo/TestTemporizador.java
@@ -0,0 +1,36 @@
+package poo;
+
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+public class TestTemporizador {
+
+	public static void main(String[] args) {
+		
+		ActionListener oyente = new CurTime(); //Se crea una instancia del tipo de la interfaz pero de un tipo que la implementa.
+		
+		Timer miTemporizador = new Timer(5000, oyente);
+		
+		miTemporizador.start();//En este punto, la ejecución se va a detener. Se debe identifica la manera de prolongar esta ejecución.
+		
+		JOptionPane.showMessageDialog(null, "Pulsa Aceptar para detener");
+		
+		System.exit(0);
+
+	}
+
+}
+
+class CurTime implements ActionListener {
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		Date curDate = new Date();
+		System.out.println("Hora cada 5 segundos " + curDate);
+		
+	}
+	
+}
