cfcdd998961d88c4e438e41189e1872a2f1e612e
xdaco <xdaco@hotmail.com>
2019-02-06 09:01:59 -0500

    Bonus 3: Testing Data Types

---
+++ pruebas/DataTypeTest.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+public class DataTypeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int testInt = 123_456;
+		
+		System.out.println(testInt);
+		
+		int var1 = 10;
+		double var2 = 2D;
+		
+		System.out.println(var2/var1);
+
+	}
+
+}
