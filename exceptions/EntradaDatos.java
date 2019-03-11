5e7cdc160ebce0ab71102ed6e09a1641eaca2614
xdaco <xdaco@hotmail.com>
2019-03-11 11:33:02 -0500

    Clase 144: Creación de métodos para captura de errores (objeto).

---
+++ exceptions/EntradaDatos.java
@@ -0,0 +1,58 @@
+package exceptions;
+
+import java.util.*;
+public class EntradaDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("¿Qué deseas hacer?");
+		System.out.println("1. Introducir datos");
+		System.out.println("2. Salir del programa");
+		
+		Scanner entrada=new Scanner (System.in);
+		
+		int decision=entrada.nextInt();
+		
+		if (decision==1){
+			
+			pedirDatos();
+			
+		}else{
+			
+			System.out.println("Adios");
+			
+			System.exit(0);
+		}
+		
+		entrada.close();
+		
+	}
+	
+		static void pedirDatos() throws InputMismatchException { //No se considera buena práctica lanzar la excepción si el código puede construirse mejor			
+		
+			try {Scanner entrada=new Scanner(System.in);
+			
+			System.out.println("Introduce tu nombre, por favor");
+			
+			String nombre_usuario=entrada.nextLine();
+			
+			System.out.println("Introduce edad, por favor");
+			
+			int edad=entrada.nextInt();	
+
+			System.out.println("Hola " + nombre_usuario + ". El año que viene tendrás " + (edad+1) + " años");
+			
+			entrada.close();
+			
+			}catch(Exception e) {//ha de capturar la excepción del throws o de una superclase
+				System.out.println("no introdujiste bien los datos");
+			}
+		
+		
+		System.out.println("Hemos terminado");
+		
+	}
+		
+		
+}
